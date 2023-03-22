7508fa68f3b4c997703b8125831855ac1856d335
Axeon <mipa0010@gmail.com>
2023-03-22 04:58:36 +0200

    Static linked sdl mixer which again took a long time only to realise I only had to set one ON to OFF but oh well. The A executable now should play a sound along opening a window for ~5s.

--- A/src/main.cpp
+++ A/src/main.cpp
@@ -1,4 +1,5 @@
 #include "SDL3/SDL.h"
+#include "SDL3/SDL_mixer.h"
 #include "box2d/box2d.h"
 
 #include <iostream>
@@ -8,8 +9,23 @@ int main(int argc, char** argv)
     SDL_Init(SDL_INIT_EVERYTHING);
 
     SDL_Window* Window = SDL_CreateWindow("Test", 700, 360, 0);
-    
+    if(Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096) != 0)
+    {
+        std::cout << "Failed to initialize Mixer: " << Mix_GetError() << "\n";
+    }
+
+    Mix_Chunk* sound = Mix_LoadWAV("resource/sound/message-pop-sound.wav");
+
+    if(!sound)
+    {
+        std::cout << "Failed to load the given WAV- file\n";
+    }
+
+    int channel = Mix_PlayChannel(-1, sound, 0);
+    // while(Mix_Playing(channel) != 0);
+
     SDL_Delay(5000);
+    
 
     b2Vec2 gravity(0.0f, -10.0f);
     b2World world(gravity);
@@ -22,6 +38,10 @@ int main(int argc, char** argv)
     
     groundBox.SetAsBox(50.0f, 10.0f);
 
+    world.DestroyBody(groundBody);
+
+    Mix_FreeChunk(sound);
+    Mix_CloseAudio();
     SDL_DestroyWindow(Window);
     SDL_Quit();
 
